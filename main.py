4d0133d84448c61503e12d42a8bb3a19ebe3ea6d
lauraDascalu <dascalulaura27@gmail.com>
2024-11-10 13:52:45 +0200

    Modified main.py

--- main.py
+++ main.py
@@ -3,18 +3,14 @@ import sys
 
 
 def main():
-    """
+
+    #if len(sys.argv) > 1 and sys.argv[1] == 'process':
     train_tsv_path = os.path.join("resources", "train_tsv")
     output_folder = os.path.join("output_vectors")
     process_tsv_files(train_tsv_path, output_folder)
-    """
-
-    if len(sys.argv) > 1 and sys.argv[1] == 'process':
-        train_tsv_path = os.path.join("resources", "train_tsv")
-        output_folder = os.path.join("output_vectors")
-        process_tsv_files(train_tsv_path, output_folder)
-    else:
-        print("No processing was performed. Pass 'process' as an argument.")
+    print("Done")
+    #else:
+        #print("No processing was performed. Pass 'process' as an argument.")
 
 
 if __name__ == '__main__':
