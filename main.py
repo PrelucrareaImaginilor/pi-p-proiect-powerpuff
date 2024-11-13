e7bac746957320ee23bcda42323fd47d43b8568e
lauraDascalu <dascalulaura27@gmail.com>
2024-11-13 15:06:01 +0200

    Implement the creation of the Feature Matrix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from processingTSV import *
 import sys
 from Laura import *
+from processingVectors import *
 
 def main():
 
@@ -16,6 +17,13 @@ def main():
     check_vector_dimensions(output_folder, num_vectors=3)
     # size of a row vector: [1, 19900], which is correct bc we had 200x200 matrices
     # the output vectors should have a dimension of 200*199/2
+
+    metadata_file = "resources/metadata/test_metadata.csv"
+    full_feature_tensor = create_feature_matrix_with_metadata(output_folder, metadata_file)
+    print("Feature Matrix created\n")
+    output_feature_matrix = os.path.join("output_feature_matrix")
+    save_feature_matrix_with_metadata(full_feature_tensor, output_feature_matrix)
+    print("Feature Matrix saved\n")
 
 if __name__ == '__main__':
     main()
