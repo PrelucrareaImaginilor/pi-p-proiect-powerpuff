82dc8d05b9e0dd9e6f8023e116bb3c59ff59370c
lauraDascalu <dascalulaura27@gmail.com>
2024-11-13 15:04:16 +0200

    Add a fucntion to extarct the participiant_id from teh filenames

--- Laura/laura.py
+++ Laura/laura.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
+import re
 
 def load_tsv(file_path):
     try:
@@ -32,3 +33,12 @@ def check_vector_dimensions(output_folder, num_vectors=2):
         vector_df = pd.read_csv(output_file, sep='\t', header=None)
         vector = torch.tensor(vector_df.values, dtype=torch.float32)
         print(f"Vector shape: {vector.shape}")
+
+#we get the participant_id for each subject's vector
+# we are looking for ex: NDARAT358XM9
+def extract_participant_id(filename):
+    match= re.search(r'NDAR[A-Z]{2}\d{3}[A-Z0-9]{3}', filename)
+    if match:
+        return match.group()
+    return None
+
