d2c479580d2f791010faf45ec77b82b3c2e2d23d
lauraDascalu <dascalulaura27@gmail.com>
2024-11-14 04:13:05 +0200

    main.py

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ def main():
         print("Output vectors are processing...")
         process_tsv_files(train_tsv_path, output_folder)
 
-    check_vector_dimensions(output_folder, num_vectors=3)
+    check_vector_dimensions(output_folder, num_vectors=1)
     # size of a row vector: [1, 19900], which is correct bc we had 200x200 matrices
     # the output vectors should have a dimension of 200*199/2
 
-    metadata_file = "resources/metadata/test_metadata.csv"
-    full_feature_tensor = create_feature_matrix_with_metadata(output_folder, metadata_file)
-    print("Feature Matrix created\n")
-    output_feature_matrix = os.path.join("output_feature_matrix")
-    save_feature_matrix_with_metadata(full_feature_tensor, output_feature_matrix)
-    print("Feature Matrix saved\n")
+    metadata_file = os.path.join("resources/metadata/training_metadata.csv")
+
+
 
 if __name__ == '__main__':
     main()
