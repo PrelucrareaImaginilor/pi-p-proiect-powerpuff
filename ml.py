c95fb2c9434117379df90008f1c3ed2877f5bf38
lauraDascalu <dascalulaura27@gmail.com>
2025-01-08 14:19:37 +0200

    updated model

--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@ import math
 
 import pandas as pd
 import os
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, RidgeCV, ElasticNet
 from sklearn.metrics import accuracy_score, r2_score
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import Lasso
@@ -11,6 +11,9 @@ from sklearn.model_selection import GridSearchCV, KFold
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.linear_model import Ridge
+from unicodedata import normalize
+from sklearn.model_selection import GridSearchCV
 
 project_dir = os.path.dirname(__file__)
 
@@ -30,114 +33,275 @@ df_test=x_test.select_dtypes(include=["number"])
 #scale the data
 scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(df)
-x_test_scaled = scaler.fit_transform(df_test)
-#y_train_final = scaler.fit_transform(y_train)
+x_test_scaled = scaler.transform(df_test)
+#y_train_scaled = scaler.fit_transform(y_train)
+
+# print(f"Mean after scaling: {np.mean(x_train_scaled, axis=0)}")
+# print(f"Variance after scaling: {np.var(x_train_scaled, axis=0)}")
+# Mean after scaling: [ 0.00000000e+00  3.70074342e-17  5.76028758e-16 ... -4.98795852e-17
+#   5.79246795e-17  6.61306758e-16]
+# Variance after scaling: [0. 1. 1. ... 1. 1. 1.]
+non_zero_variance_columns = np.var(x_train_scaled, axis=0) != 0
+
+x_train_cleaned = x_train_scaled[:, non_zero_variance_columns]
+x_test_cleaned = x_test_scaled[:, non_zero_variance_columns]
+
+
+
+from scipy.stats import zscore
+
+z_scores = np.abs(zscore(x_train_cleaned))
+outliers = (z_scores > 3).all(axis=1)
+x_train_no_outliers = x_train_cleaned[~outliers]
+
+
+
+z_scores_test = np.abs(zscore(x_test_cleaned))
+outliers_test = (z_scores_test > 3).all(axis=1)  # Flag rows with outliers
+x_test_no_outliers = x_test_cleaned[~outliers_test]
+
+
 
 X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(
-    x_train_scaled, y_train, test_size=0.2)
+    x_train_no_outliers, y_train, test_size=0.2)
+
+
+
+
+##for cross-validation
+cv=5
+
+def evaluate_model(model, xx_test, yy_test, model_name):
+    y_pred_final = model.predict(xx_test)
+    mae = mean_absolute_error(yy_test, y_pred_final)
+    mse = mean_squared_error(yy_test, y_pred_final)
+    rmse = math.sqrt(mse)
+    r2 = r2_score(yy_test, y_pred_final)
+
+    print(f"{model_name} Results:")
+    print(f"MAE: {mae}")
+    print(f"MSE: {mse}")
+    print(f"RMSE: {rmse}")
+    print(f"R^2: {r2}")
+    return mae, mse, rmse, r2
+
 
 ###########Linear regression####################
+#Mean R^2 for Linear Regression: 0.5766031088601306
+#model=LinearRegression()
 '''
-model=LinearRegression()
 model.fit(X_train_split, y_train_split)
 y_pred=abs(model.predict(X_test_split))
-print("Linear Regression")
+evaluate_model(model, X_test_split, y_test_split, "Linear Regression")
 '''
-#print(y_pred)
 '''
-# # Female only data frame
-# female_X_train = x_train_final[x_train_final['sex_Male'] == 0]
-# female_X_test = x_test_final[x_test_final['sex_Male'] == 0]
-# #Female age predictions
-# y_train_female = y_train[x_train['sex_Male'] == 0]['age']
-#
-# # Male only data frame
-# male_X_train = x_train_final[x_train_final['sex_Male'] ==  1]
-# male_X_test = x_test_final[x_test_final['sex_Male'] ==  1]
-# #Male age predictions
-# y_train_male = y_train[x_test['sex_Male'] == 1]['age']
-#
-# model.fit(female_X_train, y_train_female)
-# model.fit(male_X_train, y_train_male)
-#
-# scores_male = cross_val_score(model, male_X_train, y_train_male, cv=5)  # 5-fold cross-validation
-# # print("Cross-validated scores for Male:", scores_male)
-# scores_female = cross_val_score(model, female_X_train, y_train_female, cv=5)  # 5-fold cross-validation
-# # print("Cross-validated scores for Female:", scores_female)
-# male_avg_score = scores_male.mean()
-# female_avg_score = scores_female.mean()
-# print("Average cross-validated score for Male:", male_avg_score)
-# print("Average cross-validated score for Female:", female_avg_score)
+linear_cv_scores=cross_val_score(model, x_train_scaled, y_train.values.ravel(), cv=cv, scoring='r2')
+print(f"Linear Regression CV Scores: {linear_cv_scores}")
+print(f"Mean R^2 for Linear Regression: {linear_cv_scores.mean()}")
 '''
 
 
 ##############Lasso regression############
+# Mean R^2 for Lasso Regression: 0.6001533481959017
+model_lasso_best_param= Lasso(alpha= 0.08) #calling the model with the best parameter found: 0.08
+
+#manual splitting dataset
 '''
-#params = {"alpha": np.logspace(-3, 1, 50)}
-
-
-# Number of Folds and adding the random state for replication
-# kf=KFold(n_splits=5,shuffle=True, random_state=42)
-# # first lasso model
-# model_lasso1 = Lasso(random_state=0)
-# #define parameters dictionary
-# alpha_range=np.arange(0.07, 2, 0.1)
-# param={'alpha': alpha_range}
-# #score base on r2
-# grid_search_r2 = GridSearchCV(model_lasso1, param_grid=param, cv=5)
-# grid_search_r2.fit(X_train_split, y_train_split)
-# #scor base on mae
-# grid_search_mae=GridSearchCV(model_lasso1, param_grid=param, scoring='neg_mean_absolute_error', cv=5)
-# grid_search_mae.fit(X_train_split, y_train_split)
-
-#results
-# print('Scoring R2')
-# print('Best R2 score   : ', grid_search_r2.best_score_)
-# print('Best parameters : ', grid_search_r2.best_params_)
-# print()
-#
-#
-# # results
-# print('Scoring MAE')
-# print('Best MAE score  : ', abs(grid_search_mae.best_score_))
-# print('Best parameters : ', grid_search_mae.best_params_)
-
-# Loaded
-# Scoring R2
-# Best R2 score   :  0.6001533481959017
-# Best parameters :  {'alpha': np.float64(0.08)}
-#
-# Scoring MAE
-# Best MAE score  :  1.6199736242962175
-# Best parameters :  {'alpha': np.float64(0.08)}
-# model_lasso1.fit(x_train_final, y_train)
-# print("first lasso model prediction")
-# y_pred=model_lasso1.predict(x_test_final)
-# print(y_pred)
-#print("Best Params {}".format(model_lasso1.best_params_)) #get best alpha value
-'''
-
-# calling the model with the best parameter
-model=Lasso()
-#model_lasso_best_param= Lasso(alpha= 0.07196856730011521) #this is the alpha we got from fitting the model!
-
-model_lasso_best_param= Lasso(alpha= 0.08) #0.08 r2: 0.6139
 model_lasso_best_param.fit(X_train_split, y_train_split)
 y_pred=model_lasso_best_param.predict(X_test_split)
-print("Lasso Regression")
+evaluate_model(model_lasso_best_param, X_test_split, y_test_split, "Lasso regression with 0.08")
+'''
+#cross-validation
+
+model_lasso_best_param.fit(X_train_split, y_train.values.ravel())
+y_pred=model_lasso_best_param.predict(X_test_split)
+
+scores = cross_val_score(model_lasso_best_param, X_train_split, y_train.values.ravel(), cv=3, scoring='neg_root_mean_squared_error', n_jobs=-1)
+mean_rmse = -scores.mean()
+print(f"Mean RMSE: {mean_rmse}")
+
+important_features = np.abs(model_lasso_best_param.coef_) > 0.01  # Adjust threshold as needed
+x_train_selected = X_train_split[:, important_features]
+
+print(f"Shape after keeping important feature: {x_train_selected.shape}")
+
+x_test_selected = X_test_split[:, important_features]
 
 
 
-mae=mean_absolute_error(y_test_split, y_pred)
-mse=mean_squared_error(y_test_split, y_pred)
-rmse=math.sqrt(mse)
-r2=r2_score(y_test_split, y_pred)
 
-print(f"Validation Mean Absolute Error (MAE): {mae}")
-print(f"Validation Mean Squared Error (MSE): {mse}")
-print(f"Validation Root Mean Squared Error (RMSE): {rmse}")
-print(f"Validation R^2 Score: {r2}")
+#########Ridge regression############
+#alpha:39
+    #Mean R^2 for Ridge Regression: 0.5770820748027898
+#gridSearch alpha
+    #Mean R^2 for Ridge Regression: 0.585754748401208
+# #finding the parameter
+'''
 
+alphas = range(1,40)
+
+for a in alphas:
+    model_ridge=Ridge(alpha=a)
+    model_ridge.fit(X_train_split, y_train_split)
+
+regr_cv=RidgeCV(alphas=range(1,40))
+model_cv=regr_cv.fit(X_train_split, y_train_split)
+
+print(model_cv.alpha_)#39
+'''
+'''
+ridge_params = {'alpha': np.logspace(-3, 3, 50)}
+ridge_model = GridSearchCV(Ridge(), param_grid=ridge_params, scoring='neg_mean_squared_error', cv=5)
+ridge_cv_scores=cross_val_score(ridge_model, x_train_scaled, y_train.values.ravel(), cv=cv, scoring='r2')
+print(f"Ridge Regression CV Scores: {ridge_cv_scores}")
+print(f"Mean R^2 for Ridge Regression: {ridge_cv_scores.mean()}")
+
+ridge_model.fit(X_train_split, y_train_split)
+best_ridge = ridge_model.best_estimator_
+print(f"Best Ridge Alpha: {ridge_model.best_params_['alpha']}")
+evaluate_model(best_ridge, X_test_split, y_test_split, "Ridge Regression")
+'''
+
+'''
+model_ridge_best_param= Ridge(alpha= 39) #calling the model with the best parameter found: 0.08
+#cross-validation
+ridge_cv_scores=cross_val_score(model_ridge_best_param, x_train_scaled, y_train.values.ravel(), cv=cv, scoring='r2')
+print(f"Ridge Regression CV Scores: {ridge_cv_scores}")
+print(f"Mean R^2 for Ridge Regression: {ridge_cv_scores.mean()}")
+'''
+#model_lasso_best_param.fit(x_train_scaled, y_train.values.ravel())
+#y_pred=model_lasso_best_param.predict(x_test_scaled)
+
+
+#####Random Forest Regressor##############
+'''
+from sklearn.ensemble import RandomForestRegressor
+rf_model = RandomForestRegressor( n_estimators=500, n_jobs=-1,random_state=0)
+print("done")
+rf_cv_scores = cross_val_score(rf_model, x_train_scaled, y_train.values.ravel(), cv=3, scoring='r2')
+print(f"Random Forest CV Scores: {rf_cv_scores}")
+print(f"Mean R^2 for Random Forest: {rf_cv_scores.mean()}")
+'''
+
+########Support Vector Regression (SVR)
+#Mean R^2 for SVR Regressor: 0.3578130997955859
+'''
+from sklearn.svm import SVR
+
+svr_model = SVR(kernel='rbf', C=1.0, epsilon=0.2)
+# Cross-validation for the stacking regressor
+
+stacking_cv_scores = cross_val_score(svr_model, x_train_scaled, y_train.values.ravel(), cv=3, scoring='r2', n_jobs=-1)
+print(f"Stacking Regressor CV Scores: {stacking_cv_scores}")
+print(f"Mean R^2 for Stacking Regressor: {stacking_cv_scores.mean()}")
+'''
+#use train test split
+'''
+svr_model.fit(X_train_split, y_train_split.values.ravel())
+y_pred_split = svr_model.predict(X_test_split)
+
+# Evaluate the model
+mae = mean_absolute_error(y_test_split, y_pred_split)
+mse = mean_squared_error(y_test_split, y_pred_split)
+rmse = math.sqrt(mse)
+r2 = r2_score(y_test_split, y_pred_split)
+
+print(f"Stacking Regressor Test Results:")
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")'''
+
+#############Gradient Boosting Regressor
+'''
+#too highly computational
+from sklearn.ensemble import GradientBoostingRegressor
+
+gbr_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1)
+# Cross-validation for the stacking regressor
+
+gbr_model.fit(X_train_split, y_train_split.values.ravel())
+y_pred_split = gbr_model.predict(X_test_split)
+
+# Evaluate the model
+mae = mean_absolute_error(y_test_split, y_pred_split)
+mse = mean_squared_error(y_test_split, y_pred_split)
+rmse = math.sqrt(mse)
+r2 = r2_score(y_test_split, y_pred_split)
+
+print(f"Stacking Regressor Test Results:")
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")
+'''
+
+##K-Nearest Neighbors (KNN)
+'''
+#highly innacurate
+from sklearn.neighbors import KNeighborsRegressor
+
+knn_model = KNeighborsRegressor(n_neighbors=5)
+knn_model.fit(X_train_split, y_train_split.values.ravel())
+y_pred_split = knn_model.predict(X_test_split)
+
+# Evaluate the model
+mae = mean_absolute_error(y_test_split, y_pred_split)
+mse = mean_squared_error(y_test_split, y_pred_split)
+rmse = math.sqrt(mse)
+r2 = r2_score(y_test_split, y_pred_split)
+
+print(f"Stacking Regressor Test Results:")
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")
+'''
+
+#######ElasticNet
+# Mean R^2 for ElasticNet Regressor: 0.5745607249738547
+#Mean RMSE: 2.1588707106477916
+
+#elasticnet_model = ElasticNet(alpha=0.08, l1_ratio=0.01) #good score
+elasticnet_model = ElasticNet(alpha=0.08, l1_ratio=0.01)
+elasticnet_model.fit(x_train_selected, y_train)
+##cross-validation
+'''
+scores = cross_val_score(elasticnet_model, x_train_scaled, y_train.values.ravel(), cv=3, scoring='neg_root_mean_squared_error', n_jobs=-1)
+mean_rmse = -scores.mean()
+print(f"Mean RMSE: {mean_rmse}")
+'''
+
+y_pred_train = elasticnet_model.predict(x_train_selected)
+
+# Evaluate the model
+mae = mean_absolute_error(y_test_split, y_pred_train)
+mse = mean_squared_error(y_test_split, y_pred_train)
+rmse = math.sqrt(mse)
+r2 = r2_score(y_test_split, y_pred_train)
+
+print(f"Stacking Regressor Test Results:")
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+#submission
+#####################################
+
+'''
 y_pred_final=model_lasso_best_param.predict(x_test_scaled)
 
 
@@ -147,18 +311,8 @@ submission_df = pd.DataFrame({
 })
 if len(submission_df) != 474:
     print(f"Expected 474 rows, but got {len(submission_df)}")
-submission_file_path = os.path.join(project_dir, 'submission_lasso_0.08_iter.csv')
+submission_file_path = os.path.join(project_dir, 'submission_elasticnet3.csv')
 submission_df.to_csv(submission_file_path, index=False)
+'''
 
-
-# # Create a DataFrame of coefficients
-# names=df.columns
-# x_train_final_df = pd.DataFrame(x_train_final, columns=names)
-# # Using np.abs() to make coefficients positive.
-# lasso1_coef = np.abs(lasso1.coef_)
-# coefficients_df = pd.DataFrame({'Feature': names, 'Coefficient': lasso1_coef})
-# coefficients = coefficients_df[coefficients_df['Coefficient'] > 0.10]
-# sorted_coefficients = coefficients.sort_values(by='Coefficient', ascending=False)
-# print("\nCoefficients greater than 0.10 :")
-# print(sorted_coefficients)
 
