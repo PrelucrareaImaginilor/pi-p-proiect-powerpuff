8482e133ef8aeb608149ea5f44a2d81c9edc2caf
lauraDascalu <dascalulaura27@gmail.com>
2025-01-08 22:29:19 +0200

    update

--- elastic_net.py
+++ elastic_net.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 from sklearn.linear_model import LinearRegression, RidgeCV, ElasticNet
 from sklearn.metrics import accuracy_score, r2_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, learning_curve
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV, KFold
@@ -16,7 +16,8 @@ from unicodedata import normalize
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 
-from ml import z_scores
+from scipy.stats import zscore
+
 
 project_dir = os.path.dirname(__file__)
 
@@ -38,33 +39,26 @@ scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(df)
 x_test_scaled = scaler.transform(df_test)
 
-#remove outliers using z_score
-z_scores=np.abs(x_train_scaled)
-z_threshold=3
-outliers=(z_scores>z_threshold).any(axis=1)
-
-df_cleaned = df[~outliers]
-df_test_cleaned = df_test[~outliers]
-
-x_train_scaled_cleaned = scaler.fit_transform(df_cleaned)
-x_test_scaled_cleaned = scaler.transform(df_test_cleaned)
 
 
 X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(
-     x_train_scaled, y_train, test_size=0.2)
+      x_train_scaled, y_train, test_size=0.2)
 
-# X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(
-#     x_train_scaled_cleaned, y_train, test_size=0.2)
+
 
 #######ElasticNet
 
 # Mean R^2 for ElasticNet Regressor: 0.5745607249738547
 #Mean RMSE: 2.1588707106477916
 
-elasticnet_model = ElasticNet(alpha=0.08, l1_ratio=0.01) #good score
+elasticnet_model = ElasticNet(alpha=0.08, l1_ratio=0.01, max_iter=10000, random_state=42) #good score
     ##train test split
 elasticnet_model.fit(X_train_split, y_train_split.values.ravel())
-y_pred_train = abs(elasticnet_model.predict(X_test_split))
+
+
+#############i wrote this
+
+y_pred_train = elasticnet_model.predict(X_test_split)
     ##cross-validation
 
 print(f"Size y predicted: {y_pred_train.shape}")
@@ -100,10 +94,12 @@ plt.grid(alpha=0.3)
 
 plt.show()
 
+
 #submission
 #####################################
 
 y_pred_final=elasticnet_model.predict(x_test_scaled)
+print(f"Size y predicted final: {y_pred_final.shape}")
 
 
 submission_df = pd.DataFrame({
